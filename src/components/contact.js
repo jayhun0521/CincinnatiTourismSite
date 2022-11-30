f12d97c94cd2305a8a075e47355315ed069993aa
jayhun0521 <jayhun21@gmail.com>
2022-11-30 09:25:09 -0500

    updating repository

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,11 +2,8 @@ import React from 'react'
 
 const Contact = () => {
   return (
-<<<<<<< HEAD
-    <div>Contact Us</div>
-=======
     <div>here is the contact page</div>
->>>>>>> 80b02e6c75f77a3e991d529324aa1ffcc0e1b5c4
+
   )
 }
 
