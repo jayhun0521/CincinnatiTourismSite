cb59a07157633aa2c9d0c6ccb9e406c18069d58d
jayhun0521 <jayhun21@gmail.com>
2022-12-09 15:11:18 -0500

    updating files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,47 +1,88 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-export default class Navbar extends Component {
+function Navigation () {
     
-  render() {
     return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-         <div class="container-fluid">
-        <Link to="/" className="navbar-brand">
-          Welcome to Cincinnati
-        </Link>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="navbar-item">
-              <Link to="/" className="nav-link">
-                Home
-              </Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/about" className="nav-link">
-                About
-              </Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/viewattractions" className="nav-link">
-                Attractions
-              </Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/addattractions" className="nav-link">
-                Add Attractions
-              </Link>
-              </li>
-            <li className="navbar-item">
-              <Link to="/contact" className="nav-link">
-                Contact Us
-              </Link>
-            </li>
-          </ul>
-        </div>
-        </div>
-      </nav>
+      // <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+      //    <div class="container-fluid">
+      // <Link to="/" className="navbar-brand">
+      //   Welcome to Cincinnati
+      //  </Link>
+      //   <div className="collapse navbar-collapse">
+      //     <ul className="navbar-nav mr-auto">
+      //       <li className="navbar-item">
+      // <Link to="/" className="nav-link">
+      //   Home
+      // </Link>
+      //       </li>
+      //       <li className="navbar-item">
+      // <Link to="/about" className="nav-link">
+      //   About
+      // </Link>
+      //       </li>
+      //       <li className="navbar-item">
+      // <Link to="/viewattractions" className="nav-link">
+      //   Attractions
+      // </Link>
+      //       </li>
+      //       <li className="navbar-item">
+      // <Link to="/addattractions" className="nav-link">
+      //   Add Attractions
+      // </Link>
+      //         </li>
+      //       <li className="navbar-item">
+      // <Link to="/contact" className="nav-link">
+      //   Contact Us
+      // </Link>
+      //       </li>
+      //     </ul>
+      //   </div>
+      //   </div>
+      // </nav>
+      <Navbar bg="dark" expand="lg" style={{ color: "white" }}>
+        <Container>
+          <Navbar.Brand>
+            <Link to="/" className="navbar-brand text-light">
+              <h2>Welcome to Cincinnati</h2>
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link>
+                <Link to="/" className="nav-link text-light">
+                  Home
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/about" className="nav-link text-light">
+                  About
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/viewattractions" className="nav-link text-light">
+                  Attractions
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/addattractions" className="nav-link text-light">
+                  Add Attractions
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/contact" className="nav-link text-light">
+                  Contact Us
+                </Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
   }
-}
 
+export default Navigation;
--- src/components/home.js
+++ src/components/home.js
@@ -8,20 +8,29 @@ class Home extends Component {
       <section className='homepage'>
 
       <div className='heading'>
-        <h1 className='title'>Welcome to Cincinnati</h1>
+        <h1 className='title'>Welcome to Cincinnati Tours</h1>
+        <br/>
+        <h3><blockquote>Learn about places to go, things to do, and see in this historic American city! </blockquote></h3>
+        <br />
       </div>
       <div className="cincinnati-img">
         <img src="https://media0.giphy.com/media/KK04ChrBCXakUq7y1I/giphy.gif?cid=6c09b952dd72d3ea1147a77892e5e1fd81db2cfb60bc7afe&rid=giphy.gif&ct=g" alt="" />
       </div>
       <div className="history">
-        <h1 className='hist-title'>History</h1>
-        <p className='hist-text'>Cincinnati was founded in 1788 by Mathias Denman. Colonel Robert, and Israel Ludlow. The name Cincinnati was named by romen soldiers back in the 5th century. Since then Cincinnati has grow lots and lots to now have a population of 308,935. Since then Cincinnati has grown a reputation to be known for out Chili, Beer, and now sports team. </p>
+        <h2 className='hist-title'>History</h2>
+        <div className='hist-text'>
+        <p>Cincinnati (/ˌsɪnsɪˈnæti/ SIN-si-NAT-ee) is a city in the U.S. state of Ohio and the county seat of Hamilton County. Settled in 1788, the city is located at the northern side of the confluence of the Licking and Ohio rivers, the latter of which marks the state line with Kentucky. The city is the economic and cultural hub of the Cincinnati metropolitan area. With an estimated population of 2,256,884, it is Ohio's largest metropolitan area and the nation's 30th-largest, and with a city population of 309,317, Cincinnati is the third-largest city in Ohio and 64th in the United States. Throughout much of the 19th century, it was among the top 10 U.S. cities by population, surpassed only by New Orleans and the older, established settlements of the United States eastern seaboard, as well as being the sixth-most populous city from 1840 until 1860. </p>
+        <p>
+        Cincinnati is home to three major sports teams: the Cincinnati Reds of Major League Baseball; the Cincinnati Bengals of the National Football League; and FC Cincinnati of Major League Soccer; it is also home to the Cincinnati Cyclones, a minor league ice hockey team. The city's largest institution of higher education, the University of Cincinnati, was founded in 1819 as a municipal college and is now ranked as one of the 50 largest in the United States. Cincinnati is home to historic architecture with many structures in the urban core having remained intact for 200 years. In the late 1800s, Cincinnati was commonly referred to as the "Paris of America", due mainly to such ambitious architectural projects as the Music Hall, Cincinnatian Hotel, and Shillito Department Store. Cincinnati is the birthplace of William Howard Taft, the 27th President and former Chief Justice of the United States.
+        </p>
+        <p>- Wikipedia</p>
+        </div>
       </div>
       <div className="weather">
-        <h1 className='forcast'>Forcast</h1>
+        <h2 className='forcast'>Forecast</h2>
       </div>
       <div className="famous">
-      <h1 className='talent'>Talent that came from Cincinnati</h1>
+      <h2 className='talent'>Famous Cincinnati Natives</h2>
         <ul className='names'>
         <li><a href="https://en.wikipedia.org/wiki/Russell_Wilson">
           <span className="name"> Russell Wilson</span></a>
